a20be6d251ac0f916cfebc7cf4a32b801294e6c8
Michael Woller <wollermi000@gmail.com>
2026-04-10 15:56:14 -0700

    fix baseUrl

--- quartz.config.ts
+++ quartz.config.ts
@@ -4,7 +4,7 @@ import * as Plugin from "./quartz/plugins"
 /**
  * Quartz 4 Configuration
  *
- * See https://quartz.jzhao.xyz/configuration for more information.
+ * See https://notes.infogothic.com/configuration for more information.
  */
 const config: QuartzConfig = {
   configuration: {
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "notes.infogothic.com",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
